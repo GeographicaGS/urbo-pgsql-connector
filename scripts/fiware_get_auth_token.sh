d90acd3511edee94c3851330436f29c903b92b6c
cayetanobv <cayetano.benavent@geographica.gs>
2015-11-24 18:56:43 +0100

    updated get auth script

--- scripts/fiware_get_auth_token.sh
+++ scripts/fiware_get_auth_token.sh
@@ -1,12 +1,3 @@
 
-AUTHTOKEN=XXXXXXXXXXXXXXXXXXX
 
-curl --insecure -i https://195.235.93.224:10027/NGSI10/queryContext -s -S --header 'Content-Type: application/json' --header 'Accept: application/json' --header 'Fiware-Service: sc_smart_region_andalucia' --header 'Fiware-ServicePath: /and_sr_cdm' --header 'x-auth-token: $AUTHTOKEN' -d '
-  {"entities": [
-    {
-      "type": "test",
-      "isPattern": "true",
-      "id": "100*"
-    }
-  ]
-}'
+curl --insecure "https://195.235.93.224:15001/v3/auth/tokens" -s -i -H "Content-Type: application/json" --data @auth_fiware.json
