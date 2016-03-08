4fdcb4326d322feebdff4a272ba60a3ae3a7d050
cayetanobv <cayetano.benavent@geographica.gs>
2016-03-08 10:44:13 +0100

    Context Broker API URLs defined as constants

--- api/config.js
+++ api/config.js
@@ -1,6 +1,17 @@
 var yaml = require('js-yaml');
 var fs   = require('fs');
 
+/*
+FIWARE NGSI APIv1
+Context management using NGSI10 standard operations:
+https://github.com/telefonicaid/fiware-orion/blob/develop/doc/manuals/user/walkthrough_apiv1.md#ngsi10-standard-operations
+*/
+var URL_AUTHTK = '/v3/auth/tokens'
+var URL_QRY = '/NGSI10/queryContext'
+var URL_UDT = '/NGSI10/updateContext'
+var URL_SBC = '/NGSI10/subscribeContext'
+var URL_SBC_UPDATE = '/NGSI10/updateContextSubscription'
+
 function Config(){
 
   this._data = yaml.safeLoad(fs.readFileSync('config.yml', 'utf8'));
@@ -29,21 +40,20 @@ function Config(){
   this.getCtxBrUrls = function(optype){
     var urlCtxBrBase = this._data.contextBrokerUrls.urlCtxBrBase;
     if (optype == 'authtk'){
-      var urlAuthtk = this._data.contextBrokerUrls.urlAuthtk;
       var portAuthtk = this._data.contextBrokerUrls.portAuthtk;
-      return urlCtxBrBase + ':' + portAuthtk + urlAuthtk
+      return urlCtxBrBase + ':' + portAuthtk + URL_AUTHTK
     }
     else{
       var portCtxApi = this._data.contextBrokerUrls.portCtxApi;
       var apiBase = urlCtxBrBase + ':' + portCtxApi;
       if (optype == 'subscr')
-        return apiBase + this._data.contextBrokerUrls.urlSbc;
+        return apiBase + URL_SBC;
       else if (optype == 'updsbscr')
-        return apiBase + this._data.contextBrokerUrls.urlSbcUpdate;
+        return apiBase + URL_SBC_UPDATE;
       else if (optype == 'update')
-        return apiBase + this._data.contextBrokerUrls.urlUdt;
+        return apiBase + URL_UDT;
       else if (optype == 'updsbscr')
-        return apiBase + this._data.contextBrokerUrls.urlQry;
+        return apiBase + URL_QRY;
       else
         return null
     }
