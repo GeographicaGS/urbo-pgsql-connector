584c1371e6d56b22a34271d86fcac57bebaf6d86
Javier Muñoz <javimudi@users.noreply.github.com>
2017-01-17 15:25:11 +0100

    Basic API test

---
+++ api/test/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var request = require('supertest');
+var dispatch = require('../jobs/queue').dispatch;
+var cluster = require('cluster');
+var should = require('chai').should();  // actually call the function
+var process = require('process');
+var app = require('../app');
+
+describe('MAIN', function(){
+  it('API', function(done){
+    request(app)
+    .get('/')
+    .expect(200)
+    .end(done);
+  });
+});
