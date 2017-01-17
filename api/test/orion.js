15ec28525ebf9a39434529a8227479fcedb7f7cb
Javier Muñoz <javimudi@users.noreply.github.com>
2017-01-17 16:39:00 +0100

    Test

--- api/test/orion.js
+++ api/test/orion.js
@@ -5,6 +5,12 @@ var cluster = require('cluster');
 var should = require('chai').should();  // actually call the function
 var process = require('process');
 var app = require('../app');
+var config = require('../config');
+var getLargeSubscriptions = require('../orion/subscriptiondata')
+
+
+
+console.log(config.getCtxBrUrls('query'));
 
 describe('ORION', function(){
   it('a', function(done){
