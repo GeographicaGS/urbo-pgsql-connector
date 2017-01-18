5d4f50f4251e59dbba64c12bd769812ebbe5a96c
Javier Muñoz <javimudi@users.noreply.github.com>
2017-01-18 13:06:18 +0100

    Test

--- api/test/main.js
+++ api/test/main.js
@@ -7,13 +7,13 @@ var process = require('process');
 var app = require('../app');
 
 describe('MAIN', function(){
-  it('API', function(done){
-    request(app)
-    .get('/')
-    .expect(200)
-    .expect(function(res){
-      res.text.should.be.equal('"URBO - PGSQL Connector"')
-    })
-    .end(done);
-  });
+  // it('API', function(done){
+  //   request(app)
+  //   .get('/')
+  //   .expect(200)
+  //   .expect(function(res){
+  //     res.text.should.be.equal('"URBO - PGSQL Connector"')
+  //   })
+  //   .end(done);
+  // });
 });
