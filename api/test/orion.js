298dbf454a905789c42bce79aeeb5c32f4125d29
Javier Muñoz <javimudi@users.noreply.github.com>
2017-01-18 19:14:47 +0100

    Data testing

--- api/test/orion.js
+++ api/test/orion.js
@@ -14,143 +14,150 @@ var headers = {
 };
 
 var requestBody = {
-    url: 'http://localhost:3000/subscriptions/lighting_stcabinet_state'
-    method: 'POST',
-    type: 'connectorPsql',
-    data: {
-       title: 'lighting_stcabinet_state to PSQL',
-       "contextResponses":[
-          {
-             "contextElement":{
-                "type":"ht_6",
-                "isPattern":"false",
-                "id":"sepulveda-01",
-                "attributes":[
-                   {
-                      "name":"energyConsumed",
-                      "type":"Number",
-                      "value":"0",
-                      "metadatas":[
-                         {
-                            "name":"TimeInstant",
-                            "type":"DateTime",
-                            "value":"2017-01-17T11:30:30.828Z"
-                         },
-                         {
-                            "name":"dateUpdated",
-                            "type":"DateTime",
-                            "value":"2017-01-17T11:30:30.828Z"
-                         },
-                         {
-                            "name":"uom",
-                            "type":"Text",
-                            "value":"kVArh"
-                         }
-                      ]
-                   },
-                   {
-                      "name":"reactiveEnergyConsumed",
-                      "type":"Number",
-                      "value":"0",
-                      "metadatas":[
-                         {
-                            "name":"TimeInstant",
-                            "type":"DateTime",
-                            "value":"2017-01-17T11:30:30.837Z"
-                         },
-                         {
-                            "name":"dateUpdated",
-                            "type":"DateTime",
-                            "value":"2017-01-17T11:30:30.837Z"
-                         },
-                         {
-                            "name":"uom",
-                            "type":"Text",
-                            "value":"kVArh"
-                         }
-                      ]
-                   },
-                   {
-                      "name":"timeInstant",
-                      "type":"DateTime",
-                      "value":"2017-01-17T11:30:30.876Z"
-                   },
-                   {
-                      "name":"totalActivePower",
-                      "type":"Number",
-                      "value":"0",
-                      "metadatas":[
-                         {
-                            "name":"TimeInstant",
-                            "type":"DateTime",
-                            "value":"2017-01-17T11:30:30.818Z"
-                         },
-                         {
-                            "name":"dateUpdated",
-                            "type":"DateTime",
-                            "value":"2017-01-17T11:30:30.818Z"
-                         }
-                      ]
-                   }
-                ]
-             },
-             "statusCode":{
-                "code":"200",
-                "reasonPhrase":"OK"
-             }
-          }
-       ],
-       "subscription":{
-          "id":"lighting_stcabinet_state",
-          "subservice_id":"lighting_simulations",
-          "schemaname":"distrito_telefonica",
-          "subsduration":"P8M",
-          "substhrottling":"PT0S",
-          "fetchDataOnCreated":false,
-          "entityTypes":[
-             {
-                "typeName":"ht_6"
-             }
-          ],
-          "mode":"append",
-          "attributes":[
-             {
-                "name":"timeInstant",
-                "namedb":"TimeInstant",
-                "type":"ISO8601",
-                "indexdb":true,
-                "cartodb":true,
-                "constraint":true
-             },
-             {
-                "name":"energyConsumed",
-                "namedb":"energyconsumed",
-                "type":"float",
-                "cartodb":true
-             },
-             {
-                "name":"reactiveEnergyConsumed",
-                "namedb":"reactiveenergyconsumed",
-                "type":"float",
-                "cartodb":true
-             },
-             {
-                "name":"totalActivePower",
-                "namedb":"totalactivepower",
-                "type":"float",
-                "cartodb":true
-             }
-          ],
-          "trigger_attributes":[
-             "totalActivePower"
-          ]
-       }
-    },
-    options:{
-       attempts: 3,
-       priority: 'critical'
-    }
- }
+  type: 'connectorPsql',
+  data: {
+     title: 'lighting_stcabinet_state to PSQL',
+     "contextResponses":[
+        {
+           "contextElement":{
+              "type":"ht_6",
+              "isPattern":"false",
+              "id":"sepulveda-01",
+              "attributes":[
+                 {
+                    "name":"energyConsumed",
+                    "type":"Number",
+                    "value":"0",
+                    "metadatas":[
+                       {
+                          "name":"TimeInstant",
+                          "type":"DateTime",
+                          "value":"2017-01-17T11:30:30.828Z"
+                       },
+                       {
+                          "name":"dateUpdated",
+                          "type":"DateTime",
+                          "value":"2017-01-17T11:30:30.828Z"
+                       },
+                       {
+                          "name":"uom",
+                          "type":"Text",
+                          "value":"kVArh"
+                       }
+                    ]
+                 },
+                 {
+                    "name":"reactiveEnergyConsumed",
+                    "type":"Number",
+                    "value":"0",
+                    "metadatas":[
+                       {
+                          "name":"TimeInstant",
+                          "type":"DateTime",
+                          "value":"2017-01-17T11:30:30.837Z"
+                       },
+                       {
+                          "name":"dateUpdated",
+                          "type":"DateTime",
+                          "value":"2017-01-17T11:30:30.837Z"
+                       },
+                       {
+                          "name":"uom",
+                          "type":"Text",
+                          "value":"kVArh"
+                       }
+                    ]
+                 },
+                 {
+                    "name":"timeInstant",
+                    "type":"DateTime",
+                    "value":"2017-01-17T11:30:30.876Z"
+                 },
+                 {
+                    "name":"totalActivePower",
+                    "type":"Number",
+                    "value":"0",
+                    "metadatas":[
+                       {
+                          "name":"TimeInstant",
+                          "type":"DateTime",
+                          "value":"2017-01-17T11:30:30.818Z"
+                       },
+                       {
+                          "name":"dateUpdated",
+                          "type":"DateTime",
+                          "value":"2017-01-17T11:30:30.818Z"
+                       }
+                    ]
+                 }
+              ]
+           },
+           "statusCode":{
+              "code":"200",
+              "reasonPhrase":"OK"
+           }
+        }
+     ],
+     "subscription":{
+        "id":"lighting_stcabinet_state",
+        "subservice_id":"lighting_simulations",
+        "schemaname":"distrito_telefonica",
+        "subsduration":"P8M",
+        "substhrottling":"PT0S",
+        "fetchDataOnCreated":false,
+        "entityTypes":[
+           {
+              "typeName":"ht_6"
+           }
+        ],
+        "mode":"append",
+        "attributes":[
+           {
+              "name":"timeInstant",
+              "namedb":"TimeInstant",
+              "type":"ISO8601",
+              "indexdb":true,
+              "cartodb":true,
+              "constraint":true
+           },
+           {
+              "name":"energyConsumed",
+              "namedb":"energyconsumed",
+              "type":"float",
+              "cartodb":true
+           },
+           {
+              "name":"reactiveEnergyConsumed",
+              "namedb":"reactiveenergyconsumed",
+              "type":"float",
+              "cartodb":true
+           },
+           {
+              "name":"totalActivePower",
+              "namedb":"totalactivepower",
+              "type":"float",
+              "cartodb":true
+           }
+        ],
+        "trigger_attributes":[
+           "totalActivePower"
+        ]
+     }
+  },
+  options:{
+     attempts: 3,
+     priority: 'critical'
+  }
+}
+
+var reku = {
+  headers: headers,
+  url: config.getCtxBrUrls('subscr'),
+  method: 'POST',
+  rejectUnauthorized: false,
+  json: requestBody.data
+
+}
 
 describe('ORION', function(){
 
@@ -172,7 +179,7 @@ describe('ORION', function(){
         should.equal(error, null);
       }
 
-      request(requestBody, function(error, response, body){
+      request(reku, function(error, response, body){
         console.log(error);
         console.log(body);
         console.log(response);
