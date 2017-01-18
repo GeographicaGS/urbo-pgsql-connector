1e4cf5c587a28de2c430e3c8fe92b4f2430a7f40
Javier Muñoz <javimudi@users.noreply.github.com>
2017-01-18 18:28:13 +0100

    Skip headers test

--- api/test/orion.js
+++ api/test/orion.js
@@ -15,12 +15,12 @@ var headers = {
 
 describe('ORION', function(){
 
-  it('Dummy headers check', function(done){
-    headers['Content-Type'].should.be.equal('application/json');
-    headers['Fiware-Service'].should.be.equal('urbo');
-    headers['Fiware-ServicePath'].should.be.equal('/geographica_dev');
-    done();
-  });
+  // it('Dummy headers check', function(done){
+  //   headers['Content-Type'].should.be.equal('application/json');
+  //   headers['Fiware-Service'].should.be.equal('urbo');
+  //   headers['Fiware-ServicePath'].should.be.equal('/geographica_dev');
+  //   done();
+  // });
 
 
   it('Initialize subscriptions', function(done){
