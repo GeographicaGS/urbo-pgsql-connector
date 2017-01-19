3e10e57fc1382c41e561808021f995a01c8672f7
Javier Muñoz <javimudi@users.noreply.github.com>
2017-01-19 13:20:36 +0100

    Log

--- api/test/orion.js
+++ api/test/orion.js
@@ -128,6 +128,9 @@ describe('ORION', function(){
 
       subscriptions.initialize(function(error, data){
         should.equal(error, null);
+
+        console.log(data);
+
         request(reku, function(error, response, body){
           should.equal(error, null);
           body.contextResponses[0].statusCode.code.should.be.equal('200');
