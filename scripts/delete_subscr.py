17f6ac89d857508b8edd1b445e2c0c72f350d07a
Victor M. Varela <victor@geographica.gs>
2017-10-10 10:29:09 +0200

    licencia added

--- scripts/delete_subscr.py
+++ scripts/delete_subscr.py
@@ -1,22 +1,24 @@
 # -*- coding: utf-8 -*-
 #
-#  Author: Cayetano Benavent, 2016.
-#  cayetano.benavent@geographica.gs
-#
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
+#  Copyright 2017 Telefónica Digital España S.L.
+#  
+#  This file is part of URBO PGSQL connector.
+#  
+#  URBO PGSQL connector is free software: you can redistribute it and/or
+#  modify it under the terms of the GNU Affero General Public License as
+#  published by the Free Software Foundation, either version 3 of the
+#  License, or (at your option) any later version.
+#  
+#  URBO PGSQL connector is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#  MA 02110-1301, USA.
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+#  General Public License for more details.
+#  
+#  You should have received a copy of the GNU Affero General Public License
+#  along with URBO PGSQL connector. If not, see http://www.gnu.org/licenses/.
+#  
+#  For those usages not covered by this license please contact with
+#  iot_support at tid dot es
 #
 
 import psycopg2
