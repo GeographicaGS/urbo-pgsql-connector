b1437a46bc64f98dae3a42fb9fe204814ab360f9
José M. Camacho <hello@josemazo.com>
2017-12-13 14:22:34 +0100

    HOTFIX - Increasing limit body

--- api/app.js
+++ api/app.js
@@ -1,20 +1,20 @@
 // Copyright 2017 Telefónica Digital España S.L.
-// 
+//
 // This file is part of URBO PGSQL connector.
-// 
+//
 // URBO PGSQL connector is free software: you can redistribute it and/or
 // modify it under the terms of the GNU Affero General Public License as
 // published by the Free Software Foundation, either version 3 of the
 // License, or (at your option) any later version.
-// 
+//
 // URBO PGSQL connector is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
 // General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU Affero General Public License
 // along with URBO PGSQL connector. If not, see http://www.gnu.org/licenses/.
-// 
+//
 // For those usages not covered by this license please contact with
 // iot_support at tid dot es
 
@@ -34,8 +34,8 @@ var log = log4js.getLogger(logParams.output);
 app.use(log4js.connectLogger(log, logParams.access));  // Morgan substitute
 log.info('Access logger successfully started');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: false }));
 
 app.use('/', routes);
 app.use('/subscriptions',subscriptions.routes());
