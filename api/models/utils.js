3284cd362dd208721307a275154c55f23802af59
Álvaro Rodríguez <rguezmartinalvaro@gmail.com>
2019-07-19 15:01:07 +0200

    URL type - Added replaces for ' and \n symbols

--- api/models/utils.js
+++ api/models/utils.js
@@ -185,7 +185,10 @@ module.exports.getValueForType = function(value, type, outcome){
     return value;
   } else if (type === 'url') {
     var replace = {
-      "+": "%20"
+      "+": "%20",
+      "\'": "''",
+      "%27": "''",
+      "%0A": "\n"
     };
     value = value+'';
     for (var key in replace) {
