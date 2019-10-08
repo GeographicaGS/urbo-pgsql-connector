fbad722b2961c5e610ae1c2a0b66cfd10cab3bf0
rbsolis <bob.solis@gmail.com>
2019-10-08 18:50:09 +0200

    Added overwrite functionality for dates.

--- api/models/subscriptionscartodbmodel.js
+++ api/models/subscriptionscartodbmodel.js
@@ -1,20 +1,20 @@
 // Copyright 2017 Telefónica Digital España S.L.
-// 
+//
 // This file is part of URBO PGSQL connector.
-// 
+//
 // URBO PGSQL connector is free software: you can redistribute it and/or
 // modify it under the terms of the GNU Affero General Public License as
 // published by the Free Software Foundation, either version 3 of the
 // License, or (at your option) any later version.
-// 
+//
 // URBO PGSQL connector is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
 // General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU Affero General Public License
 // along with URBO PGSQL connector. If not, see http://www.gnu.org/licenses/.
-// 
+//
 // For those usages not covered by this license please contact with
 // iot_support at tid dot es
 
@@ -357,10 +357,18 @@ SubscriptionsCartoDBModel.prototype.storeData = function(sub,contextResponses,cb
         var attrOutcome = ('outcome' in attrSub) ? attrSub.outcome : undefined;
         if (valid_attrs.indexOf(attr.name)!=-1){
           var value = attr.value;
+          // special overwrite cases for dates
+          if ('condition' in attrSub && 'when_not' in attrSub.condition && 'then' in attrSub.condition && attrSub.type === "ISO8601") {
+            value = attrSub.condition.when_not !== value ? new Date().toISOString() : value;
+          }
+          if ('condition' in attrSub && 'when' in attrSub.condition && 'then' in attrSub.condition && attrSub.type === "ISO8601") {
+            value = attrSub.condition.when === value ? new Date().toISOString() : value;
+          }
+          // default overwrite case
           if ('condition' in attrSub && 'when' in attrSub.condition && 'then' in attrSub.condition) {
             value = attrSub.condition.when === value ? attrSub.condition.then : value;
           }
-          var value = utils.getValueForType(value, attrType, attrOutcome);
+          value = utils.getValueForType(value, attrType, attrOutcome);
           var name = (attrType !== 'coords' && !attrType.startsWith('geojson')) ? attrName : 'the_geom';
 
           if (value == null) {
